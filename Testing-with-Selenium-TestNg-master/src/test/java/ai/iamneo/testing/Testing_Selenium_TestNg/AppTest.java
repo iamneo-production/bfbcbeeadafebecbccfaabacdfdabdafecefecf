3aefe9fa565c3357bd201f9fe3c52d1ab5db19b5
Iamneo <techadmin@iamneo.ai>
2023-08-07 13:33:56 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg-master/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg-master/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -1,36 +1,68 @@
 package ai.iamneo.testing.Testing_Selenium_TestNg;
 
+import org.testng.annotations.Test;
 import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
 public class AppTest {
 
-	ChromeOptions chromeOptions = new ChromeOptions();
-	WebDriver driver = null;
+ ChromeOptions chromeOptions = new ChromeOptions();
+ WebDriver driver = null;
 
-	@BeforeTest
-	public void beforeTest() throws Exception {
-	//	System.setProperty("webdriver.chrome.driver", "/home/coder/project/workspace/Testing-with-Selenium-TestNg/chromedriver");
-		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
-	}
+ @BeforeTest
+ public void beforeTest() throws Exception
+  {
+  driver = new RemoteWebDriver(new URL("http://localhost:8080"), chromeOptions);
+     driver.manage().window().maximize();
+ }
 
-	@Test
-	public void TestCase_1() throws InterruptedException {
-		driver.get("https://admin.pscollege841.exam.ly/");
-		Thread.sleep(5000);  
-		String title = driver.getTitle();
-		Assert.assertEquals(title, "PS College");
-	}
-	@Test
-	public void TestCase_2() throws InterruptedException {
-		driver.get("https://admin.pscollege841.exam.ly/");
-		Thread.sleep(5000);  
-		String title = driver.getTitle();
-		Assert.assertEquals(title, "PS College");
-	}
+ @Test
+//Checking the title of iamNeo (Home - iamneo)
+ public void iamNeo() throws InterruptedException 
+ {
+        driver.navigate().to("http://iamneo.ai");
+  // String title =driver.getTitle();
+  // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+ }
+ @Test
+//Moving to FACEBOOK
+ public void nextPage() throws InterruptedException 
+ {
+     driver.navigate().to("https://www.facebook.com");
+  // String title =driver.getTitle();
+  // Assert.assertEquals(title, "Facebook â€“ log in or sign up");
 
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
-	}
+ }
+ @Test
+//Back to iamNeo
+ public void backPage() throws InterruptedException 
+ {
+  driver.navigate().back();
+  //String title = driver.getTitle();
+  // Assert.assertEquals(title, "Learning and assessment solution for Universities and Enterprises");
+
+ }
+ @Test
+//Current URL
+public void currentURL() throws InterruptedException 
+{
+   String title =driver.getCurrentUrl();
+   System.out.println(title);
+  // Assert.assertEquals(title, "");
+  driver.navigate().forward();
+  driver.navigate().refresh();
+
+}
+
+ @AfterTest
+ public void afterTest() 
+ {
+  driver.quit();
+ }
 
 }
